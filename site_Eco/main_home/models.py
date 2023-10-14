8326439785f4ce49918edcd41adcfafd9c2457c5
realg0d <filatov_02@inbox.ru>
2023-10-14 23:46:17 +0300

    simple push

--- site_Eco/main_home/models.py
+++ site_Eco/main_home/models.py
@@ -4,7 +4,7 @@ class Plant(models.Model):
     name_plant = models.CharField(max_length=200)
     type_plant = models.CharField(max_length=50)
     price_plant = models.IntegerField()
-    img_plant = models.ImageField()
+    img_plant = models.ImageField(blank=True, upload_to='media')
     text_plant = models.TextField()
 
     def __str__(self):
